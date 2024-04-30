3cb256bee4e3f4082884b3c8dbcd726d0fb3a2b3
rmarbeck <rafoufou>
2024-04-30 09:22:44 +0200

    Day 9 final

--- day9/src/main/scala/Solution.scala
+++ day9/src/main/scala/Solution.scala
@@ -14,33 +14,40 @@ end Solution
 
 def removeErasedCharsAndCount(input: String) : (Int, Int) =
   case class State(input: String, inGarbage: Boolean, garbagedCount: Int, groupsCount: Int, deepness: Int):
-    lazy val isEmpty: Boolean = input.isEmpty
-    lazy val head: Char = input(0)
+    type FinalResult = (Int, Int)
+    type IntermediateComputation = (Boolean, Char)
+    lazy val currentStatus: Either[IntermediateComputation, FinalResult] =
+      isFinal match
+        case true => Right(result)
+        case false => Left(current)
+    lazy val isFinal: Boolean = input.isEmpty
+    lazy val result: FinalResult = (groupsCount, garbagedCount)
+    lazy val current: IntermediateComputation = (inGarbage, input(0))
     lazy val next: State = copy(input = input.tail)
-    def skip1Char: State = next
-    def skip2Chars: State = next.next
-    def openGarbage: State = copy(inGarbage = true).next
-    def closeGarbage: State = copy(inGarbage = false).next
-    def garbageInc: State = copy(garbagedCount = garbagedCount + 1).next
-    def openGroup: State = copy(groupsCount = groupsCount + deepness, deepness = deepness + 1).next
-    def closeGroup: State = copy(deepness = deepness - 1).next
+    lazy val skip1Char: State = next
+    lazy val skip2Chars: State = next.next
+    lazy val openGarbage: State = next.copy(inGarbage = true)
+    lazy val closeGarbage: State = next.copy(inGarbage = false)
+    lazy val garbageInc: State = next.copy(garbagedCount = garbagedCount + 1)
+    lazy val openGroup: State = next.copy(groupsCount = groupsCount + deepness, deepness = deepness + 1)
+    lazy val closeGroup: State = next.copy(deepness = deepness - 1)
 
   object State:
     def init(input: String): State = State(input, false, 0, 0, 1)
 
   @tailrec
   def eraseAndCount(state: State): (Int, Int) =
-    state.isEmpty match
-      case true => (state.groupsCount, state.garbagedCount)
-      case false =>
-        (state.inGarbage, state.head) match
+    state.currentStatus match
+      case Right(result) => result
+      case Left(onGoingStatus) =>
+        onGoingStatus match
           case (false, '{') => eraseAndCount(state.openGroup)
           case (false, '}') => eraseAndCount(state.closeGroup)
           case (false, '<') => eraseAndCount(state.openGarbage)
           case (false, ',') => eraseAndCount(state.skip1Char)
           case (true, '!') => eraseAndCount(state.skip2Chars)
           case (true, '>') => eraseAndCount(state.closeGarbage)
-          case (true, char) => eraseAndCount(state.garbageInc)
+          case (true, _) => eraseAndCount(state.garbageInc)
           case _ => throw Exception("Not supported")
 
   eraseAndCount(State.init(input))
