dab73193fef0e75e625cf5cb26c95cb8757d91ff
Marbeck <raphael@watchnext.fr>
2024-04-30 07:57:09 +0200

    Day 9 OK refactoring

--- day9/src/main/scala/Solution.scala
+++ day9/src/main/scala/Solution.scala
@@ -3,47 +3,44 @@ import scala.annotation.tailrec
 object Solution:
   def run(inputLines: Seq[String]): (String, String) =
 
-    val (inputPart1, resultPart2) = removeErasedCharsAndCount(inputLines.head)
+    val (resultPart1, resultPart2) = removeErasedCharsAndCount(inputLines.head)
 
-    val result1 = s"${countGroups(inputPart1)}"
+    val result1 = s"$resultPart1"
     val result2 = s"$resultPart2"
 
     (s"${result1}", s"${result2}")
 
 end Solution
 
-def removeErasedCharsAndCount(input: String) : (String, Int) =
-  case class State(input: String, inGarbage: Boolean, outsideGarbage: StringBuilder, garbagedCount: Int):
-    def skip1Char = this.copy(input = input.tail)
-    def skip2Chars = this.copy(input = input.drop(1).tail)
-    def openGarbage = this.copy(input = input.tail, inGarbage = true)
-    def closeGarbage = this.copy(input = input.tail, inGarbage = false)
-    def appendChar(char: Char) = this.copy(input = input.tail, outsideGarbage = outsideGarbage.append(char))
-    def count = this.copy(input = input.tail, garbagedCount = garbagedCount + 1)
+def removeErasedCharsAndCount(input: String) : (Int, Int) =
+  case class State(input: String, inGarbage: Boolean, garbagedCount: Int, groupsCount: Int, deepness: Int):
+    lazy val isEmpty: Boolean = input.isEmpty
+    lazy val head: Char = input(0)
+    lazy val next: State = copy(input = input.tail)
+    def skip1Char: State = next
+    def skip2Chars: State = next.next
+    def openGarbage: State = copy(inGarbage = true).next
+    def closeGarbage: State = copy(inGarbage = false).next
+    def garbageInc: State = copy(garbagedCount = garbagedCount + 1).next
+    def openGroup: State = copy(groupsCount = groupsCount + deepness, deepness = deepness + 1).next
+    def closeGroup: State = copy(deepness = deepness - 1).next
 
   object State:
-    def init(input: String) = State(input, false, StringBuilder(), 0)
+    def init(input: String): State = State(input, false, 0, 0, 1)
 
   @tailrec
-  def eraseAndCount(state: State): (String, Int) =
-    state.input.isEmpty match
-      case true => (state.outsideGarbage.toString, state.garbagedCount)
+  def eraseAndCount(state: State): (Int, Int) =
+    state.isEmpty match
+      case true => (state.groupsCount, state.garbagedCount)
       case false =>
-        (state.inGarbage, state.input(0)) match
+        (state.inGarbage, state.head) match
+          case (false, '{') => eraseAndCount(state.openGroup)
+          case (false, '}') => eraseAndCount(state.closeGroup)
           case (false, '<') => eraseAndCount(state.openGarbage)
           case (false, ',') => eraseAndCount(state.skip1Char)
-          case (false, char) => eraseAndCount(state.appendChar(char))
           case (true, '!') => eraseAndCount(state.skip2Chars)
           case (true, '>') => eraseAndCount(state.closeGarbage)
-          case (true, char) => eraseAndCount(state.count)
+          case (true, char) => eraseAndCount(state.garbageInc)
+          case _ => throw Exception("Not supported")
 
   eraseAndCount(State.init(input))
-
-@tailrec
-def countGroups(input: String, level: Int = 1, counter: Int = 0): Int =
-  input.isEmpty match
-    case true => counter
-    case false =>
-      input(0) match
-        case '{' => countGroups(input.tail, level + 1, counter + level)
-        case '}' => countGroups(input.tail, level - 1, counter)
