06b74b208bd215919d5745b34573d5aeb16072ee
Marbeck <raphael@watchnext.fr>
2024-05-04 12:14:54 +0200

    Day 17 OK

---
+++ day17/src/main/scala/Solution.scala
@@ -0,0 +1,46 @@
+object Solution:
+  def run(inputLines: Seq[String]): (String, String) =
+
+    val part1Limit = 2017
+    val part2Limit = 50_000_000
+
+    val nbOfSteps = inputLines.head.toInt
+
+    val resultPart1 = process(CircularBuffer.init, part1Limit, nbOfSteps).positionAfter(part1Limit)
+
+    val resultPart2 = atFirstPosition(nbOfSteps).span(_ < part2Limit)._1.toList.last
+
+    val result1 = s"$resultPart1"
+    val result2 = s"$resultPart2"
+
+    (s"${result1}", s"${result2}")
+
+end Solution
+
+def atFirstPosition(nbOfSteps: Int, from: Int = 0, currentPosition: Int = 0, currentSize: Int = 1): LazyList[Int] =
+  val newPosition = (currentPosition + nbOfSteps) % currentSize + 1
+  newPosition match
+    case 1 => currentSize #:: atFirstPosition(nbOfSteps, from + 1, 1, currentSize + 1)
+    case value => atFirstPosition(nbOfSteps, from + 1, value, currentSize + 1)
+
+
+def process(buffer: CircularBuffer, remainingSteps: Int, nbOfSteps: Int, current: Int = 1, currentPosition: Int = 0): CircularBuffer =
+  remainingSteps match
+    case 0 => buffer
+    case _ =>
+      val (newBuffer, newCurrentPosition) = buffer.insert(currentPosition, nbOfSteps)
+      process(newBuffer, remainingSteps - 1, nbOfSteps, current + 1, newCurrentPosition)
+
+case class CircularBuffer(values: List[Int]):
+  override def toString: String = values.mkString(",")
+  def positionAfter(value: Int): Int =
+    values.indexOf(value) match
+      case -1 => throw Exception("Not found")
+      case index => values((index + 1) % values.size)
+
+  def insert(fromPosition: Int, nbOfSteps: Int): (CircularBuffer, Int) =
+    val positionToInsert = (fromPosition + nbOfSteps) % values.size + 1
+    (CircularBuffer(values.take(positionToInsert) ++: (values.size +: values.drop(positionToInsert))), positionToInsert)
+
+object CircularBuffer:
+  val init: CircularBuffer = CircularBuffer(List(0))
